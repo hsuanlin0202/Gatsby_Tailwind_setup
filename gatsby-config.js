111d8ad1fd10b3b9f62952d947ea0632d97d5c19
sherin.lin <sherin.lin@car-plus.com.tw>
2021-03-09 18:39:02 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-postcss",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const init = {
+  margin: 0,
+  padding: 0,
+};
+function test() {
+  return (
+    <main className="bg-blue-700 text-white" style={init}>
+      test
+    </main>
+  );
+}
+
+export default test;
